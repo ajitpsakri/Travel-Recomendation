130a037e84cc79b35a3f7fd4341dfe734dcbdcf3
Ajit <62175113+ajitpsakri@users.noreply.github.com>
2021-07-31 22:48:38 +0530

    style: added background to App.js

--- src/components/display.js
+++ src/components/display.js
@@ -2,8 +2,15 @@ const display = (ele) => {
   return ele.places.map((item) => {
     return (
       <div key={item.name} className="display">
-        <img src={item.link} alt={item.name} />
-        <h3>{item.name}</h3>
+        <div className="section-image">
+          <img src={item.link} alt={item.name} />
+        </div>
+        <div className="section-rating">
+          <small>{item.rating}</small>
+        </div>
+        <div className="Section-name">
+          <h3>{item.name}</h3>
+        </div>
       </div>
     );
   });
--- src/db/hubli.js
+++ src/db/hubli.js
@@ -12,7 +12,7 @@ const hubliDoc = [
         name: "Nrupatunga Betta",
         link:
           "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/2b/ee/b7/playing-area-for-kids.jpg?w=300&h=300&s=1",
-        ratin: "4.5/5"
+        rating: "4.5/5"
       }
     ]
   },
